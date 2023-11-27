fcabc2eb85971d6ef234a99711a7d0124cff5cc6
Chris <hereokay88@gmail.com>
2023-11-27 15:18:35 +0900

    fix: Repository 에노테이션 추가

--- src/main/java/com/example/demo/repository/EthereumPriceRepository.java
+++ src/main/java/com/example/demo/repository/EthereumPriceRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.EthereumPrice;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EthereumPriceRepository extends MongoRepository<EthereumPrice, String> {
     EthereumPrice findFirstByTimeStampLessThanEqualOrderByTimeStampDesc(Long timeStamp);
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User,String> {
     Optional<User> findByAddress(String address);
     List<User> findAllByOrderBySpendGasUSDTDesc();
